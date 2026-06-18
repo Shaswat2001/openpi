226337a988f18f17eac2f36cf467e6625e26ff72
Shaswat2001 <sis_shaswat@outlook.com>
2026-06-18 17:36:06 +0000

    feat: add new models

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -23,6 +23,7 @@ import openpi.policies.bridge_policy as bridge_policy
 import openpi.policies.haply_policy as haply_policy
 import openpi.policies.fractal_policy as fractal_policy
 import openpi.policies.libero_policy as libero_policy
+import openpi.policies.metaworld_policy as metaworld_policy
 import openpi.shared.download as _download
 import openpi.shared.normalize as _normalize
 import openpi.training.droid_rlds_dataset as droid_rlds_dataset
@@ -425,6 +426,51 @@ class RLDSDroidDataConfig(DataConfigFactory):
             datasets=self.datasets,
         )
 
+@dataclasses.dataclass(frozen=True)
+class LeRobotMetaworldDataConfig(DataConfigFactory):
+    extra_delta_transform: bool = False
+
+    @override
+    def create(
+        self, assets_dirs: pathlib.Path, model_config: _model.BaseModelConfig
+    ) -> DataConfig:
+        repack_transform = _transforms.Group(
+            inputs=[
+                _transforms.RepackTransform(
+                    {
+                        "observation/image": "observation.image",
+                        "observation/state": "observation.state",
+                        "actions": "action",
+                        "prompt": "prompt",
+                    }
+                )
+            ]
+        )
+
+        data_transforms = _transforms.Group(
+            inputs=[
+                metaworld_policy.MetaworldInputs(model_type=model_config.model_type)
+            ],
+            outputs=[metaworld_policy.MetaworldOutputs()],
+        )
+
+        if self.extra_delta_transform:
+            delta_action_mask = _transforms.make_bool_mask(6, -1)
+            data_transforms = data_transforms.push(
+                inputs=[_transforms.DeltaActions(delta_action_mask)],
+                outputs=[_transforms.AbsoluteActions(delta_action_mask)],
+            )
+
+        model_transforms = ModelTransformFactory()(model_config)
+
+        return dataclasses.replace(
+            self.create_base_config(assets_dirs, model_config),
+            repack_transforms=repack_transform,
+            data_transforms=data_transforms,
+            model_transforms=model_transforms,
+            action_sequence_keys=("action",),
+        )
+    
 @dataclasses.dataclass(frozen=True)
 class LeRobotBridgeDataConfig(DataConfigFactory):
     use_quantile_norm: bool = True
@@ -1074,6 +1120,36 @@ _CONFIGS = [
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
         num_train_steps=20_000,
         batch_size=64,
+    ),
+    TrainConfig(
+        name="pi0_metaworld",
+        model=pi0_config.Pi0Config(action_horizon=5),
+        data=LeRobotMetaworldDataConfig(
+            repo_id="lerobot/metaworld_mt50",
+            base_config=DataConfig(prompt_from_task=True),
+            assets=AssetsConfig(assets_dir="checkpoints/torch/pi0_metaworld/assets"),
+            extra_delta_transform=False,
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader(
+            "checkpoints/jax/pi0_base/params"
+        ),
+        pytorch_weight_path="checkpoints/torch/pi0_base",
+    ),
+    TrainConfig(
+        name="pi05_metaworld",
+        model=pi0_config.Pi0Config(
+            pi05=True, action_horizon=5, discrete_state_input=False
+        ),
+        data=LeRobotMetaworldDataConfig(
+            repo_id="lerobot/metaworld_mt50",
+            base_config=DataConfig(prompt_from_task=True),
+            assets=AssetsConfig(assets_dir="checkpoints/torch/pi0_metaworld/assets"),
+            extra_delta_transform=False,
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader(
+            "checkpoints/jax/pi05_base/params"
+        ),
+        pytorch_weight_path="checkpoints/torch/pi05_base",
     ),
     #
     # Fine-tuning DROID configs.
